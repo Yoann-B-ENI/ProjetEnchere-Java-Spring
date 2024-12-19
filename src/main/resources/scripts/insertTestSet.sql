d79c1ec2c5fe82a54928aac5f65e35f58afc87a7
Yoann-B-ENI <yoann.battu2024@campus-eni.fr>
2024-12-19 14:49:21 +0100

    fix (DB): make insert logical

--- src/main/resources/scripts/insertTestSet.sql
+++ src/main/resources/scripts/insertTestSet.sql
@@ -41,19 +41,19 @@ VALUES
 ('Laptop', 'A high-performance laptop with 16GB RAM and 512GB SSD', '2025-01-02', '2025-01-30', 
 500, 500, 'Created', 1, null, 1, 1),  -- John Doe is the vendor (idVendor 1)
 ('Sofa', 'Comfortable 3-seater sofa with fabric upholstery', '2024-12-05', '2024-12-29', 
-300, 300, 'AuctionStarted', 2, null, 2, 2),  -- Jane Smith is the vendor (idVendor 2)
+300, 581, 'AuctionStarted', 2, null, 2, 2),  -- Jane Smith is the vendor (idVendor 2)
 ('Novel - "The Great Adventure"', 'A thrilling novel about an epic journey', '2024-12-02', '2024-12-09', 
-20, 80, 'AuctionEnded', 4, 2, 3, 3);  -- Bob Jones is the vendor (idVendor 3)
+20, 36, 'AuctionEnded', 4, 2, 3, 3);  -- Bob Jones is the vendor (idVendor 3)
 
 -- Insert Bids
 INSERT INTO bids (idMember, idArticle, bidDate, bidPrice)
 VALUES 
-(2, 2, '2024-12-06', 550),  -- Jane Smith bidding on the Laptop
-(4, 2, '2024-12-07', 551),  -- Bob Jones bidding on the Laptop
-(2, 2, '2024-12-08', 580),  -- Jane Smith bidding on the Laptop
-(4, 2, '2024-12-09', 581),  -- Bob Jones bidding on the Laptop
-(4, 3, '2024-12-06', 36),   -- Bob Jones bidding on the Novel
+(2, 2, '2024-12-06', 350),  -- Jane Smith bidding on the Laptop
+(4, 2, '2024-12-07', 351),  -- Bob Jones bidding on the Laptop
+(2, 2, '2024-12-08', 380),  -- Jane Smith bidding on the Laptop
+(4, 2, '2024-12-09', 381),  -- Bob Jones bidding on the Laptop
 (1, 3, '2024-12-04', 25);   -- John Doe bidding on the Novel
+(4, 3, '2024-12-06', 36),   -- Bob Jones bidding on the Novel
 
 
 
